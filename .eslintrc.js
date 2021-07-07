0dc86d86bc4174acbc57ca63a258a3d0ae84bb7e
Edward Faulkner <ef@alum.mit.edu>
2021-07-07 16:29:30 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  plugins: ['node', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended'],
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['src/**/*.js', '__tests__/**/*.js'],
+  rules: {},
+  overrides: [
+    // test files
+    {
+      files: ['__tests__/**/*.js'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        'node/no-unpublished-require': 'off',
+      },
+    },
+    {
+      parserOptions: {
+        ecmaVersion: 2020,
+      },
+      files: ['**/*.ts'],
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        'prefer-const': 'off',
+        'node/no-unsupported-features/es-syntax': [
+          'error',
+          {
+            ignores: ['modules'],
+          },
+        ],
+        'node/no-missing-import': 'off',
+
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        'node/no-extraneous-import': 'off',
+
+        // We should try to remove this eventually
+        '@typescript-eslint/explicit-function-return-type': 'off',
+
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              // we currently use `object` as "valid WeakMap key" in a lot of APIs
+              object: false,
+            },
+          },
+        ],
+
+        // disabling this one because of DEBUG APIs, if we ever find a better
+        // way to suport those we should re-enable it
+        '@typescript-eslint/no-non-null-assertion': 'off',
+
+        '@typescript-eslint/no-use-before-define': 'off',
+      },
+    },
+  ],
+};
---
+++ __tests__/test-support.ts
@@ -0,0 +1,94 @@
+import 'jest';
+import { transform as transform7, TransformOptions as Options7 } from '@babel/core';
+import { createContext, Script } from 'vm';
+
+interface RunDefaultOptions {
+  dependencies?: { [name: string]: any };
+}
+
+export function toCJS(code: string): string {
+  return transform7(code, {
+    plugins: [
+      require.resolve('@babel/plugin-transform-modules-commonjs'),
+      require.resolve('@babel/plugin-transform-typescript'),
+    ],
+  })!.code!;
+}
+
+export function runDefault(code: string, opts: RunDefaultOptions = {}): any {
+  let cjsCode = toCJS(code);
+
+  function myRequire(name: string): any {
+    if (opts.dependencies && opts.dependencies[name]) {
+      return opts.dependencies[name];
+    }
+    return require(name);
+  }
+
+  let context = createContext({
+    exports: {},
+    require: myRequire,
+  });
+  let script = new Script(cjsCode);
+  script.runInContext(context);
+  return context.exports.default();
+}
+
+function presetsFor(_major: 7) {
+  return [
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        modules: false,
+        targets: {
+          ie: '11.0.0',
+        },
+      },
+    ],
+  ];
+}
+
+export interface Transform {
+  (code: string, opts?: { filename?: string }): string;
+  babelMajorVersion: 6 | 7;
+  usingPresets: boolean;
+}
+
+export function allBabelVersions(params: {
+  babelConfig(major: 7): Options7;
+  createTests(transform: Transform): void;
+  includePresetsTests?: boolean;
+}) {
+  function versions(usePresets: boolean) {
+    describe('babel7', function () {
+      function transform(code: string, opts?: { filename?: string }) {
+        let options7: Options7 = params.babelConfig(7);
+        if (!options7.filename) {
+          options7.filename = 'sample.js';
+        }
+        if (usePresets) {
+          options7.presets = presetsFor(7);
+        }
+        if (opts && opts.filename) {
+          options7.filename = opts.filename;
+        }
+
+        return transform7(code, options7)!.code!;
+      }
+      transform.babelMajorVersion = 7 as 7;
+      transform.usingPresets = usePresets;
+      params.createTests(transform);
+    });
+  }
+
+  if (params.includePresetsTests) {
+    describe('with presets', function () {
+      versions(true);
+    });
+    describe('without presets', function () {
+      versions(false);
+    });
+  } else {
+    versions(false);
+  }
+}
